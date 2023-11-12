19d116b20992e495b88614783331b9b978216de8
Samuel Hernandez <71301115+elsamird003@users.noreply.github.com>
2023-11-12 10:34:02 -0500

    changed the icon, tittle and api key

--- Ueat/src/Welcome.tsx
+++ Ueat/src/Welcome.tsx
@@ -3,15 +3,15 @@ import React, { } from "react";
 import {LoginProps } from "./types";
 import "./Welcome.css"
 import Ueat_logo from "./Ueat_logo.png";
+
 const Welcome: React.FC<LoginProps> = ({ setLoggedIn }) => {
     return(
         <>
     <div className = "container">
       <h1 className="welcome">Welcome</h1>
-      <img src = {Ueat_logo} className = 'logo' onClick={() => setLoggedIn(true)}></img>
-      <div className="card">
-      </div>
-      </div>
+      <img alt = "logo"src = {Ueat_logo} className = 'logo' onClick={() => setLoggedIn(true)}></img>
+          
+ </div> 
       </>
      );
 };
